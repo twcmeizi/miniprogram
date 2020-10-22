45c0065f7384723153b128e9c529ebd9b2091091
twcmeizi <2673443077@qq.com>
2020-10-22 16:58:19 +0800

    第二次提交

--- pages/test/test.js
+++ pages/test/test.js
@@ -25,34 +25,13 @@ Page({
         left=(res.screenWidth - 50)/2
       }
     })
-    // zenjia test 
-    let query = wx.createSelectorQuery();
-    query.select('.weui-tabs-bar__content').boundingClientRect();
-    query.exec((res) => {console.log(res)
-      // let listHeight = res[0].height; // 获取list高度
-        // wx.getSystemInfo({
-        //   success: function (height) {
-        //     that.setData({
-        //       scrollHeight: parseInt(height.windowHeight) - 64 - listHeight 
-        //     });
-        //   }
-        // });
-    })
     const titles = ['推荐', '音乐', '风景', '祝福', '生活', '开心']
-    // const tabs = titles.map(item => ({
-    //   title: item
-    // }))
     that.requestData(1,10);
-   
     this.setData({
       tabs:titles,
       windowHeight,
       left
-      
     })
-  },
-  onReady() {
-    //this.videoContext = wx.createVideoContext('myVideo')
   },
   requestData(page,count) {
     if(!count){
@@ -61,24 +40,20 @@ Page({
     wx.request({
       url: 'https://api.apiopen.top/getJoke?page='+page+'&count='+count+'&type=video',
       success: (result) => {
-        console.log(result.data.result)
         result.data.result.forEach(n => {
           n.isPlaying = false;
         })
         this.setData({
           contentList:result.data.result
         })
-        console.log(this.data.contentList)
       }
     })
   },
-  
   gotoDetail(e){
     wx.navigateTo({
       url: '../testdetail/testdetail?sid='+e.currentTarget.dataset.sid,
     })
   },
-
   onTabClick(e) {
     const index = e.detail.index
     this.setData({
@@ -88,7 +63,6 @@ Page({
     console.log(index)
     this.requestData(e.detail.index+1,10)
   },
-
   onChange(e) {
     const index = e.detail.index
     this.setData({
@@ -107,7 +81,6 @@ Page({
     this.requestData(1,20)
   },
   refresh(e){
-   
     wx.request({
       url: 'https://api.apiopen.top/getJoke?page='+1+'&count='+5+'&type=video',
       success: (result) => {
